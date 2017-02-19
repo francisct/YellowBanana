42911c4a13578cb147013e6f680079ec41a62e29
francisct <franciscotetremblay@gmail.com>
2017-02-19 01:45:22 -0500

    api.js was not compliant with es5

--- webapp/server/routes/api.js
+++ webapp/server/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET api listing. */
-router.get('/', (req, res) => {
+router.get('/', function(req, res) {
   res.send('api works');
 });
 
